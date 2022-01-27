dc3463397e949e9a5dd0d16550c22668be01931e
elfiel20 <98498077+elfiel20@users.noreply.github.com>
2022-01-26 19:02:53 -0500

    Add files via upload

---
+++ Madlib.py
@@ -0,0 +1,58 @@
+#Hello this is my first attempt at makng a Madlib
+
+print("This is a madlib game!")
+print("Please follow the instructions below to create a fun version of a stanz in: ")
+print("J.cole's No Role Modelz")
+
+#I tried various iterations of the line spacing on the lines above
+
+print("Enter a Verb:")
+answer1=input()
+
+#I  realize that I can encourage the player via appearances of text in between entries
+print("Great!")
+
+print("Now Please Enter a noun:")
+answer2=input()
+
+print("That's a fun one, Hahaha.")
+
+#I am having fun giving the narrator a voice/personality.
+#The coding format here is pretty simple
+
+print("Now give me a number, any number!")
+answer3=input()
+
+print("Well now we're on track to be over in a jiffy.")
+
+print("What about another one of those nouns? Don't be afraid to get creative!")
+answer4=input()
+
+print("This is looking pretty good if I do say so myself!")
+
+
+print("This here is your final result. I hope it'll make you chuckle.")
+print(""
+      ""
+      ""
+      ""
+      ""
+      ""
+)
+
+#The above was a guess on how to add line spaces in my code as to make the last bit a little less cluttered
+#Upon running it it seems to work
+
+
+
+#This is the final formatting for the madlib
+print(answer1, "me one time shame on you")
+print(answer1, answer2,"twice can't put the blame on you.")
+print(answer1, answer2, answer3, "times", answer4, "the peace sign and let it rain on you")
+
+
+#Code by enmity
+
+
+
+
